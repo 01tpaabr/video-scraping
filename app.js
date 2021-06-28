32b303b6430380d72168a3526538fd99db970781
Thiago Andrade <01tpaa.br@gmail.com>
2021-06-28 16:16:42 -0300

    intercepting hls requests and storing links

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
 const pptr = require('puppeteer-core');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
-const { input } = require('./readline_util.js')
+const { input } = require('./readline_util.js');
+const { request } = require('http');
 const configFilePath = "./config.json"
 const config = require(configFilePath);
-
-async function launchBrowser(profileMode){
-    try{
-        if(profileMode){
-            //Needs chrome process to be closed before launching
-            return await pptr.launch({
-                headless: config.headlessMode,
-                executablePath: config.executablePath,
-                userDataDir: config.userData
-            });
-
-        }else{
-            return await pptr.launch({headless: config.headlessMode});
-        }
-    }catch(e){
-        console.log("Error while launching browser");
-        console.log(e);
-    }
-}
 
 async function downloadYoutubeVideo(url, mode, count){
     try{
@@ -37,25 +19,76 @@ async function downloadYoutubeVideo(url, mode, count){
     }
 }
 
-if(config.profileMode && (config.userData === "" || config.userDataDir === "")){
-    console.log("Please fill in user data");
+function updateVideoCount(filepath, file, newVideos){
+    file.videoCount += newVideos;
+    fs.writeFile(filepath, JSON.stringify(file), function writeJSON (e){
+        if(e) return console.log(e);
+    });
 }
+
+//Page obj, url and regex pattern to filter requests urls
+async function getHLSRequests(page, url, pattern){
+    let requests = [];
+
+    await page.setRequestInterception(true);
+
+    page.on('request', req => {
+        req.continue();
+    });
+
+    page.on('response', response => {
+        //intercepts m3u8 types
+        if(response.url().match(pattern)){
+            requests.push(response.url());
+        }
+    });
+
+    await page.goto(url, { waitUntil: 'networkidle0' });
+
+    await page.setRequestInterception(false);
+
+    return requests;
+}
+
 
 //Main
 (async () => {
-    const browser = await launchBrowser(config.profileMode);
+    const browser = await pptr.launch({
+        headless: config.headlessMode,
+        executablePath: config.executablePath,
+        userDataDir: config.userData,
+        defaultViewport: { width: 1600, height: 800 }
+    });
+
     const [page] = await browser.pages();
 
-    let url = await input("Url do video: ");
-    await page.goto(url);
-    
-    //Download video
-    await downloadYoutubeVideo(url, "audio", config.videoCount);
-    config.videoCount += 1;
+    let url = await input("Url:");
 
-    //Update video Count in json file
-    fs.writeFile(configFilePath, JSON.stringify(config), function writeJSON (e){
-        if(e) return console.log(e);
-    });
-    
+    //Intercepting hls streams
+    let requests = await getHLSRequests(page, url, /(m3u8)/);
+    console.log(requests);
+
+    // await page.setRequestInterception(true);
+
+    // page.on('request', req => {
+    //     req.continue();
+    // });
+
+    // page.on('response', response => {
+    //     //intercepts m3u8 types
+    //     let pattern = /(m3u8)/
+    //     if(response.url().match(/(m3u8)/)){
+    //         requests.push(response.url());
+    //     }
+    // });
+
+    // await page.goto(url, { waitUntil: 'networkidle0' });
+
+    // console.log(requests);
+
+    //await page.goto(url);
+    //Download video
+    //await downloadYoutubeVideo(url, "audio", config.videoCount);
+    //updateVideoCount(configFilePath, config, 1);
+    //https://cursos.alura.com.br/course/certificacao-ccna-parte-1/task/26518
 })();
